da1f92e05459a65d5e36d78059a65c10083a8f93
pinkyjohnny <136741239+pinkyjohnny@users.noreply.github.com>
2023-08-10 16:48:37 +0300

    task-09

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,20 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+const colorChahgeBtn = document.querySelector(".change-color");
+const colorName = document.querySelector(".color");
+const backgroundColor = document.querySelector("body")
+
+
+colorChahgeBtn.addEventListener('click', onColorChangeBtnClick)
+
+function onColorChangeBtnClick() {
+  const randomColor = getRandomHexColor();
+  backgroundColor.style.backgroundColor = randomColor;
+  colorName.textContent = randomColor;
+}
+
+
+
